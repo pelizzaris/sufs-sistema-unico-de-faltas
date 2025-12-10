f9ded2f3804ba20cb4cd7398a79a330fed9352a4
pelizzaris <vinipelizzari2410@gmail.com>
2025-12-10 00:35:59 -0300

    :construction: Criação de DTOs para a entidade Aluno

--- src/main/java/com/pelizzaris/sufs/domain/model/Aluno.java
+++ src/main/java/com/pelizzaris/sufs/domain/model/Aluno.java
@@ -1,10 +1,6 @@
 package com.pelizzaris.sufs.domain.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
